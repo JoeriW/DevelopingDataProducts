e78dbfd40ca189ff2a6f6ea9a55010d8145c6f58
JoeriW <willems.joeri85@telenet.be>
2015-06-21 10:07:26 +0200

    commitment

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ library(shiny)
 shinyUI(pageWithSidebar(
         headerPanel("Circumference of a circle"),
         sidebarPanel(
-                sliderInput("enterRadius","Enter radius of the circle:",value=1,min=0,max=100,step=1),
+                sliderInput("enterRadius","Enter radius of the circle:",value=0,min=0,max=100,step=1),
                 h6("Circumference of the circle equals"),
                 verbatimTextOutput("Circum")
         ),
